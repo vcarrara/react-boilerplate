9281dda43d4151e038864311981cfce7ae1d023d
vcarrara <victor.carrara@utt.fr>
2020-05-20 17:19:20 +0200

    Add .postcssrc.json

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import Greeting from './Greeting'
 import './index.css'
 
 const App = () => (
-    <>
+    <div className="App">
         <Greeting firstName="John" lastName="Doe" />
         <small>react-boilerplate</small>
-    </>
+    </div>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -27,16 +27,7 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [autoprefixer()],
-                        },
-                    },
-                ],
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
