ad02f8c6e1a37c71644e8eb3776aa73732866fcf
vcarrara <victor.carrara@utt.fr>
2020-05-20 11:34:45 +0200

    add postcss support with autoprefixer and pollyfills

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+import './style/index.css'
+
 const MyComponent = () => <h1>Hello from react-boilerplate!</h1>
 
 ReactDOM.render(<MyComponent />, document.getElementById('root'))
