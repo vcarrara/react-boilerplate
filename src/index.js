ce3d26a7c9cb84b33e5cbb0355b97f47803a630e
vcarrara <victor.carrara@utt.fr>
2020-05-20 14:09:05 +0200

    Refactor index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-import './style/index.css'
+import './index.css'
 
 const MyComponent = () => <h1>Hello from react-boilerplate!</h1>
 
